971c749c2d2a01a16e4a04313e426e464f675c2e
ttwag <taowang@ucdavis.edu>
2026-08-03 02:36:22 +0000

    Added Black Formatter

--- csim.py
+++ csim.py
@@ -1,5 +1,5 @@
-
 import numpy as np
+
 # note:
 # number of node and voltage source are provided at the beginning of the net list file
 # first node of voltage source in net list is the positive terminal
@@ -7,13 +7,14 @@ import numpy as np
 # array index = node index - 1
 # n = row, m = col
 
+
 def fill_array(A_matrix, z_vector, line, n, m):
     component = line[0][0]
     component_num = int(line[0][1:]) - 1
-    node_A = int(line[1]) - 1 #correct index
-    node_B = int(line[2]) - 1 
+    node_A = int(line[1]) - 1  # correct index
+    node_B = int(line[2]) - 1
     match component:
-        case 'V':
+        case "V":
             voltage = int(line[3])
             # e vector
             z_vector[n + component_num] = voltage
@@ -24,15 +25,15 @@ def fill_array(A_matrix, z_vector, line, n, m):
             if node_B >= 0:
                 A_matrix[node_B][n + component_num] = -1
                 A_matrix[n + component_num][node_B] = -1
-        case 'I':
+        case "I":
             # i vector
             current = int(line[3])
             if node_A >= 0:
                 z_vector[node_A] += current
             if node_B >= 0:
                 z_vector[node_B] -= current
-        case 'R':
-            conductance = 1/int(line[3])
+        case "R":
+            conductance = 1 / int(line[3])
             # G matrix
             if node_A >= 0:
                 A_matrix[node_A][node_A] += conductance
@@ -44,6 +45,7 @@ def fill_array(A_matrix, z_vector, line, n, m):
         case _:
             print("Error")
 
+
 with open("netlist.txt", "r") as file:
     lines = file.readlines()
     n = int(lines[0][5])
@@ -51,7 +53,7 @@ with open("netlist.txt", "r") as file:
 
     A_matrix = np.zeros(shape=(n + m, n + m), dtype=float)
     z_vector = np.zeros(shape=(n + m, 1))
-    
+
     for line_num in range(3, len(lines)):
         processed_line = lines[line_num].split()
         print(processed_line)
@@ -60,4 +62,3 @@ with open("netlist.txt", "r") as file:
     print(z_vector)
 
     print(np.linalg.solve(A_matrix, z_vector))
-    
